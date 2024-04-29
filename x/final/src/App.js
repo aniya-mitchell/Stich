9c6bab3765a3e64de31b683732e536ba5c347aca
aniya-mitchell <mitchella2021@fau.edu>
2024-04-29 15:19:48 -0400

    y

--- x/final/src/App.js
+++ x/final/src/App.js
@@ -1,58 +1,47 @@
 import React from 'react';
-import './App.css'
-import { Link } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
-import Layout from './routes/layout.js'
+import './App.css';
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import './index.css';
+import Layout from './routes/layout.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FeedView from './routes/feedView.js'
-import CreateView from './routes/createView.js'
-import PostView from './routes/postView.js'
-import EditView from './routes/editView.js'
-
-
+import FeedView from './routes/feedView.js';
+import CreateView from './routes/createView.js';
+import PostView from './routes/postView.js';
+import EditView from './routes/editView.js';
 
 function App() {
   let element = useRoutes([
     {
-      path:"/feed" ,
-      element:<FeedView />
+      path: "/feed",
+      element: React.createElement(FeedView, null)
     },
     {
-      path:"/feed/edit/:id",
-      element: <EditPost data={posts} />
+      path: "/feed/edit/:id",
+      element: React.createElement(EditPost, { data: posts })
     },
     {
-      path:"/create-post",
-      element: <CreatePost />
+      path: "/create-post",
+      element: React.createElement(CreatePost, null)
     },
     {
-      path:"/feed/edit/:id",
-      element: <PostView />
+      path: "/feed/edit/:id",
+      element: React.createElement(PostView, null)
     }
-    
   ]);
 
   return (
-    <>
-      <div className='App'>
-        <h1 style={{ margin: '.5em' }}>Post Your Yarn Projects</h1>
-        <h3>Share your favorite projects, stictches, products, ect with people just like you.</h3>
-
-  
-        <div className="card">
-          <Link to={'/feed'}>
-            <button>
-              Head to Stichify Feed
-            </button>
-          </Link>
-        </div>
-      </div>
-
-
-    </>
-  )
+    React.createElement(React.Fragment, null,
+      React.createElement("div", { className: 'App' },
+        React.createElement("h1", { style: { margin: '.5em' } }, "Post Your Yarn Projects"),
+        React.createElement("h3", null, "Share your favorite projects, stitches, products, etc with people just like you."),
+        React.createElement("div", { className: "card" },
+          React.createElement(Link, { to: '/feed' },
+            React.createElement("button", null, "Head to Stichify Feed")
+          )
+        )
+      )
+    )
+  );
 }
-
-export default App
