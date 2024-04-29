2d44f7c58570f57006c0cfd524e7b7e80b9674b4
aniya-mitchell <mitchella2021@fau.edu>
2024-04-29 15:12:17 -0400

    push

--- x/final/src/App.js
+++ x/final/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import './App.css'
 import { Link } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+import Layout from './routes/layout.js'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FeedView from './routes/feedView.js'
+import CreateView from './routes/createView.js'
+import PostView from './routes/postView.js'
+import EditView from './routes/editView.js'
+
 
 
 function App() {
+  let element = useRoutes([
+    {
+      path:"/feed" ,
+      element:<FeedView />
+    },
+    {
+      path:"/feed/edit/:id",
+      element: <EditPost data={posts} />
+    },
+    {
+      path:"/create-post",
+      element: <CreatePost />
+    },
+    {
+      path:"/feed/edit/:id",
+      element: <PostView />
+    }
+    
+  ]);
 
   return (
     <>
