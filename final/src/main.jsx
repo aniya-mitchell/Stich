dea7b23801ef675e474e96ce2c90fd3655dc3f18
aniya-mitchell <mitchella2021@fau.edu>
2024-04-29 18:36:11 -0400

    push

--- final/src/main.jsx
+++ final/src/main.jsx
@@ -3,27 +3,51 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Layout from './routes/layout.jsx'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useRoutes } from "react-router-dom";
 import FeedView from './routes/feedView.jsx'
 import CreateView from './routes/createView.jsx'
 import PostView from './routes/postView.jsx'
 import EditView from './routes/editView.jsx'
 
+const element = useRoutes([
+  {
+    path: "/",
+    element: <Layout>
+      <App />
+    </Layout>
+  },
+  {
+    path: "/feed",
+    element: <Layout>
+      <FeedView />
+    </Layout>
+  },
+  {
+    path: "/create-post",
+    element: <Layout>
+      <CreateView />
+    </Layout>
+  },
+  {
+    path: "/feed/post/:id",
+    element: <Layout>
+      <PostView />
+    </Layout>
+  },
+  {
+    path: "/feed/edit/:id",
+    element: <Layout>
+      <EditView />
+    </Layout>
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
-          <Route index={false} element={<Layout />}>
-            <Route index={true} element={<App />} />
-            <Route index={false} path="/feed" element={<FeedView />} />
-            <Route index={false} path='/create-post' element={<CreateView />} />
-            <Route index={false} path="/feed/post/:id" element={<PostView />} />
-            <Route index={false} path="/feed/edit/:id" element={<EditView />} />
-          </Route>
-        </Route>
+        {element}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )
-
