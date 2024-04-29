57059346aeeea228b1a5382d8d8e49e3cf04f8c2
aniya-mitchell <mitchella2021@fau.edu>
2024-04-29 15:24:15 -0400

    j

--- x/final/src/routes/layout.js
+++ x/final/src/routes/layout.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from '../components/navbar'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from '../components/navbar';
 
 export default function Layout() {
-    return (
-        <div className='app-container'>
-            <NavBar></NavBar>
-            <Outlet />
-        </div>
-    )
+    return React.createElement('div', { className: 'app-container' },
+        React.createElement(NavBar, null),
+        React.createElement(Outlet, null)
+    );
 }
