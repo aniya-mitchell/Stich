1b5c558912fbf0de30cb454f142261c242e44860
aniya-mitchell <mitchella2021@fau.edu>
2024-04-30 20:17:53 -0400

    push

---
+++ final/webpack.configure.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Name of the output bundle
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'], // Use Babel for JavaScript/JSX files
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'], // Use file-loader for image files
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Path to your HTML template
+      filename: 'index.html', // Output HTML filename
+    }),
+  ],
+};
